c2e0968289e987e464d6ca98f94ddd4ee2af8693
Mashal <azhar.mashal@gmail.com>
2018-06-01 16:54:56 -0400

    Added home and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'bundle/install'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "home/index"
+  root "home#index"
 
-  root to: "home#index"
+  resources :users
+  resources :posts
+  resources :articles
 end
