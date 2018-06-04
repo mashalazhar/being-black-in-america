e7b7c325ee6a70cab009140516fd675f9209b6f5
Mashal <azhar.mashal@gmail.com>
2018-06-04 16:30:02 -0400

    Added styling things

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "home/index"
-  root "home#index"
+    devise_scope :user do
+      authenticated :user do
+      #modify to set the right homepage
+      root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 
   resources :users
   resources :posts
